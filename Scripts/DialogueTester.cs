7b8435c573281917161df29f1aa77b29baca517a
Allen Jake Polintan <jakepolintanart@gmail.com>
2026-05-24 12:57:03 -0700

    Download Dialogue Manager Plugin

---
+++ Scripts/DialogueTester.cs
@@ -0,0 +1,26 @@
+using Godot;
+using DialogueManagerRuntime;
+using System;
+using System.Threading.Tasks;
+
+
+public partial class DialogueTester : Node
+{
+	private Resource dialogue = GD.Load<Resource>("res://Dialogue/test.dialogue");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//function that starts the dialgoue
+		DialogueManager.ShowDialogueBalloon(dialogue, "start");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	public async Task AskForName()
+	{
+	  GD.Print("AMONG US");
+	}
+}
