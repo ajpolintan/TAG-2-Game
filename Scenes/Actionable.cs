8778100cb33a849ccf853f07482a9ce3ab656854
Allen Jake Polintan <jakepolintanart@gmail.com>
2026-05-24 16:15:01 -0700

    Created Basic Dialogue

--- Scenes/Actionable.cs
+++ Scenes/Actionable.cs
@@ -5,19 +5,29 @@ using System.Threading.Tasks;
 
 public partial class Actionable : Area2D
 {
+	//Dialogue Resources
 	[Export] public Resource DialogueResource;
 	[Export] public string DialogueString = "start";
 	
+	
+	
+	//Connecting Sign
 	public override void _Ready() {
 		BodyEntered += OnBodyEntered;
 	}
 	// goal when player enters this area, you can press space to activate space
 	public void Action()
 	{
+		GD.Print("action!");
 		DialogueManager.ShowDialogueBalloon(DialogueResource, DialogueString);
 	}
 	
 	private void OnBodyEntered(Node2D body) {
 		GD.Print("Collided");
+		if (Input.IsActionPressed("interact")) {
+			GD.Print("button pressed");
+			Action();
+		}
+		//if player presses the space button, the action function should activate
 	}
 }
--- Scripts/CharacterMovement.cs
+++ Scripts/CharacterMovement.cs
@@ -7,6 +7,11 @@ public partial class CharacterMovement : CharacterBody2D
 	/// Handles the speed of the character movement
 	/// </summary>
 	
+	/// <summary>
+	/// ActionableFinder
+	/// </summary>
+	private Area2D _actionableFinder; 
+	
 	public float Speed;
 	
 	[Export]
@@ -15,8 +20,21 @@ public partial class CharacterMovement : CharacterBody2D
 	[Export]
 	public float RunSpeed { get; set; }
 	
+	public override void _Ready() {
+		_actionableFinder = GetNode<Area2D>("ActionableFinder");
+	}
+	
 	//Get Input Function
 	public void GetInput() {
+		 if (Input.IsActionJustPressed("interact")) {
+			var actionables = _actionableFinder.GetOverlappingAreas();
+				GD.Print(actionables.Count);
+				if (actionables.Count > 0) {
+					Actionable action = (Actionable) actionables[0];
+					action.Action();
+					return;
+				}
+		 }
 		 Vector2 input_direction = Input.GetVector("left", "right", "up", "down");
 		 Velocity = input_direction * Speed;
 	} 
